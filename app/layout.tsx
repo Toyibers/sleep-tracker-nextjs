942a5c1d60ffeb23a8a8a28d72b0570937e66707
Moch Toyib <mochtoyib118@gmail.com>
2026-06-09 23:03:43 +0700

    Add footer, guest, about, and contact pages with corresponding components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
     </ClerkProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-export default function HomePage() {
+import Guest from "@/components/Guest";
+import { currentUser } from  "@clerk/nextjs/server";
+
+export default async function HomePage() {
+  const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  }
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">Sleep Tracker</h1>
